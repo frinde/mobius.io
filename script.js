676cd369b80b920787ee054cba973b521b77295d
frinde <141473374+frinde@users.noreply.github.com>
2023-09-24 15:51:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function scrolltoright() {
     var scroll = document.getElementById("scrollable");
     scroll.scrollLeft += 300
 }
+
+function sortBoxGrid() {
+    document.getElementById("grid").style.display = "flex";
+    document.getElementById("table").style.display = "none";
+}
+
+function sortBoxtable() {
+    document.getElementById("grid").style.display = "none";
+    document.getElementById("table").style.display = "block";
+}
